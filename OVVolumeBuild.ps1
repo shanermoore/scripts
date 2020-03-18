650d05c372cc611c34cf4be665c7f76bc78c7eed
Shane <smoore1976@hotmail.com>
2020-03-18 09:23:23 -0500

    Update OVVolumeBuild.ps1

--- OVVolumeBuild.ps1
+++ OVVolumeBuild.ps1
@@ -1,16 +1,16 @@
 Write-host "Enter OneView password at the prompt" -ForegroundColor Yellow
  Connect-HPOVMgmt -Hostname "" -UserName ""
- $VC = Write-Host "Enter vCenter name/ip to connect to" -ForegroundColor Cyan
+ $VC = Read-Host "Enter vCenter name/ip to connect to" -ForegroundColor Cyan
  Connect-viserver $VC
  
  Get-HPOVStoragePool
  Write-host "For the next two questions refer to the data on screen" -ForegroundColor Yellow
- $Pool = Write-host "Enter the name of the Storage Pool to use for the new volume" -ForegroundColor Cyan
- $System = Write-Host "Enter the name of the Storage System that Pool is on" -ForegroundColor Cyan
- $cap = Write-host "Enter the size of the volume in GB" -ForegroundColor Cyan
- $name = Write-Host "Enter the name of the new volume" -ForegroundColor Cyan
- $ID = Write-Host "Enter the Lun ID fo rthe new volume" -ForegroundColor Cyan
- $cluster = Write-Host "Enter VMware cluster to add datastore"
+ $Pool = Read-host "Enter the name of the Storage Pool to use for the new volume" 
+ $System = Read-Host "Enter the name of the Storage System that Pool is on"
+ $cap = Read-host "Enter the size of the volume in GB"
+ $name = Read-Host "Enter the name of the new volume"
+ $ID = Read-Host "Enter the Lun ID fo rthe new volume"
+ $cluster = Read-Host "Enter VMware cluster to add datastore"
 
  $SP = Get-HPOVStoragePool -Name $Pool -StorageSystem $System
  New-HPOVStorageVolume -Name $name -StoragePool $SP -Capacity $cap -ProvisioningType Thin
